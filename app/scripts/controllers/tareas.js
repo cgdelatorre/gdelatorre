2b477e1176d1518c9ce60934f7df1ebc8c43e95f
cgdelatorr <gdelatorre@cetys.edu.mx>
2016-08-20 15:04:39 -0700

    initial commit

---
+++ app/scripts/controllers/tareas.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gdelatorreApp.controller:TareasCtrl
+ * @description
+ * # TareasCtrl
+ * Controller of the gdelatorreApp
+ */
+angular.module('gdelatorreApp')
+  .controller('TareasCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
